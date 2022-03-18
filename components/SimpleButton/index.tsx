d0fc6d167649de86c6771f13623bf53afc2c2b31
Dan Woodson <daniel.woodson@fisglobal.com>
2022-03-18 13:44:06 -0600

    Creating "SimpleButton" that uses React, but not MUI to test diff scenarios

---
+++ components/SimpleButton/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const SimpleButton = ({ labelText }: {labelText: string}) => (
+  <button>
+    {labelText}
+  </button>
+)
+
+export default SimpleButton
--- stories/Button1.stories.jsx
+++ stories/Button1.stories.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { Button } from '../components/Button/index';
+import { SimpleButton } from '../components/SimpleButton/index';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Spruce/Button',
-  component: Button,
+  title: 'Spruce/SimpleButton',
+  component: SimpleButton,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -13,7 +13,7 @@ export default {
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <Button {...args} />;
+const Template = (args) => <SimpleButton {...args} />;
 
 export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
