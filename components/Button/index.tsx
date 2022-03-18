14adc40b6bd3c0dcd37c4fbc39665c742f9c69db
Dan Woodson <daniel.woodson@fisglobal.com>
2022-03-18 13:26:34 -0600

    Testing with non-MUI button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import MuiButton from '@mui/material/Button'
+// import MuiButton from '@mui/material/Button'
 
 export const Button = ({ labelText }: {labelText: string}) => (
-  <MuiButton variant="contained" size="large">
+  <button>
     {labelText}
-  </MuiButton>
+  </button>
 )
 
 export default Button
