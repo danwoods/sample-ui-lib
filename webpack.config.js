845f379bab6b79b0f2fc1d2cafc9d1c73f7e6771
Dan Woodson <daniel.woodson@fisglobal.com>
2022-03-18 15:20:02 -0600

    Adding 'babel-preset-react' and removing the react import from the MUI button

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,24 @@ const glob = require('glob')
 const path = require('path')
 const fs = require('fs')
 
-const isFile = (fileName) => {
-  return fs.lstatSync(fileName).isFile()
-}
+// const isFile = (fileName) => {
+//   return fs.lstatSync(fileName).isFile()
+// }
 
-console.log(
-  fs.readdirSync(path.resolve(__dirname, './components')).map((fileName) => {
-    return path.join(path.resolve(__dirname, './components'), fileName)
-  })
-  // .filter(isFile)
-)
+// console.log(
+//   fs.readdirSync(path.resolve(__dirname, './components')).map((fileName) => {
+//     return path.join(path.resolve(__dirname, './components'), fileName)
+//   })
+//   // .filter(isFile)
+// )
 
-console.log(
-  glob.sync('./components/**/index.tsx').reduce((acc, path) => {
-    const entry = path.replace('/index.tsx', '')
-    acc[entry] = path
-    return acc
-  }, {})
-)
+// console.log(
+//   glob.sync('./components/**/index.tsx').reduce((acc, path) => {
+//     const entry = path.replace('/index.tsx', '')
+//     acc[entry] = path
+//     return acc
+//   }, {})
+// )
 
 module.exports = {
   mode: 'development',
@@ -29,14 +29,10 @@ module.exports = {
     return acc
   }, {}),
   output: {
-    // module: true,
     filename: './[name]/index.js',
     path: path.resolve(__dirname, './dist'),
     library: {
-      // libraryTarget: 'commonjs',
-      // name: 'Spruce',
       type: 'umd',
-      // export: 'default'
     }
   },
   module: {
